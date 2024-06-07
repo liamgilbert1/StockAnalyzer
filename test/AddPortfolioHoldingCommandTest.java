7994cebaf4904d43d1d6b01f984c09a0f60bc5f3
Joseph-Carlucci <148010329+Joseph-Carlucci@users.noreply.github.com>
2024-06-07 15:49:26 -0400

    StockDataPointTest

--- test/AddPortfolioHoldingCommandTest.java
+++ test/AddPortfolioHoldingCommandTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 public class AddPortfolioHoldingCommandTest {
 
   @Before
-  public void setUp() throws Exception {
+  public void setUp() {
   }
 
   @Test
--- test/StockDataPointTest.java
+++ test/StockDataPointTest.java
@@ -1,23 +1,21 @@
-import org.junit.Before;
 import org.junit.Test;
 
+import static model.stock.StockDataPoint.CLOSE;
+import static model.stock.StockDataPoint.DATE;
+import static model.stock.StockDataPoint.HIGH;
+import static model.stock.StockDataPoint.LOW;
+import static model.stock.StockDataPoint.OPEN;
+import static model.stock.StockDataPoint.VOLUME;
 import static org.junit.Assert.*;
 
 public class StockDataPointTest {
-
-  @Before
-  public void setUp() throws Exception {
-  }
-
   @Test
   public void getIndex() {
-  }
-
-  @Test
-  public void values() {
-  }
-
-  @Test
-  public void valueOf() {
+    assertEquals(0, DATE.getIndex());
+    assertEquals(1, OPEN.getIndex());
+    assertEquals(2, HIGH.getIndex());
+    assertEquals(3, LOW.getIndex());
+    assertEquals(4, CLOSE.getIndex());
+    assertEquals(5, VOLUME.getIndex());
   }
 }
