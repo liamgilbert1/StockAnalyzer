f33834039e4360e26924b04d6ec9f8af96c78244
liamgilbert1 <gilbert.li@northeastern.edu>
2024-06-07 18:53:29 -0400

    Stock documentations

--- src/model/stock/IStock.java
+++ src/model/stock/IStock.java
@@ -3,27 +3,89 @@ package model.stock;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * This interface represents a stock in the stock market simulator.
+ */
 public interface IStock {
+  /**
+   * Gets the ticker of the stock.
+   * @return the ticker of the stock
+   */
   String getTicker();
 
+  /**
+   * Checks if the stock contains the given date.
+   * @param date the date to check
+   * @return true if the stock contains the date, false otherwise
+   */
   boolean checkContainsDates(LocalDate date, int days);
 
+  /**
+   * Checks if the stock contains the given date range.
+   * @param startDate the start date to check
+   * @param endDate the end date to check
+   * @return true if the stock contains the date range, false otherwise
+   */
   boolean checkContainsDateRange(LocalDate startDate, LocalDate endDate);
 
+  /**
+   * Gets the most recent date of the stock.
+   * @return the most recent date of the stock
+   */
   LocalDate getMostRecentDate();
 
+
+  /**
+   * Gets the open price of the stock.
+   * @param date the date of the open price
+   * @return the open price of the stock
+   */
   double getOpenPrice(LocalDate date);
 
+  /**
+   * Gets the close price of the stock.
+   * @param date the date of the close price
+   * @return the close price of the stock
+   */
   double getClosePrice(LocalDate date);
 
+  /**
+   * Gets the high price of the stock.
+   * @param date the date of the high price
+   * @return the high price of the stock
+   */
   double getHighPrice(LocalDate date);
 
+  /**
+   * Gets the low price of the stock.
+   * @param date the date of the low price
+   * @return the low price of the stock
+   */
   double getLowPrice(LocalDate date);
 
+  /**
+   * Gets the volume of the stock.
+   * @param date the date of the volume
+   * @return the volume of the stock
+   */
   double getVolume(LocalDate date);
 
+  /**
+   * Gets the data across the given number of days.
+   * @param endDate the end date of the data
+   * @param days the number of days to get data across
+   * @param dataPoint the data point to get
+   * @return the data across the given number of days
+   */
   List<String> getDataAcrossDays(LocalDate endDate, int days,
                                  StockDataPoint dataPoint);
 
+  /**
+   * Gets the data across the given number of days.
+   * @param startDate the start date of the data
+   * @param endDate the end date of the data
+   * @param dataPoint the data point to get
+   * @return the data across the given number of days
+   */
   List<String> getDataAcrossDays(LocalDate startDate, LocalDate endDate, StockDataPoint dataPoint);
 }
--- src/model/stock/Stock.java
+++ src/model/stock/Stock.java
@@ -7,78 +7,160 @@ import java.util.Objects;
 import controller.readers.CSVReader;
 import controller.readers.IStockReader;
 
+/**
+ * This class represents a stock.
+ * A stock has a ticker and can be used to get stock data.
+ */
 public class Stock implements IStock {
   private final String ticker;
 
+  /**
+   * Constructs a stock object with the given ticker.
+   * @param ticker the ticker of the stock
+   */
   public Stock(String ticker) {
     this.ticker = ticker;
   }
 
+  /**
+   * Gets the ticker of the stock.
+   * @return the ticker of the stock
+   */
   @Override
   public String getTicker() {
     return ticker;
   }
 
+  /**
+   * Gets the reader for the stock.
+   * @return the reader for the stock
+   */
   protected IStockReader getReader() {
     return new CSVReader(ticker);
   }
 
+  /**
+   * Checks if the stock contains the given date.
+   * @param date the date to check
+   * @param days the number of days to check
+   * @return true if the stock contains the date, false otherwise
+   */
   @Override
   public boolean checkContainsDates(LocalDate date, int days) {
     return getReader().checkContainsDates(date, days);
   }
 
+  /**
+   * Checks if the stock contains the given date range.
+   * @param startDate the start date to check
+   * @param endDate the end date to check
+   * @return true if the stock contains the date range, false otherwise
+   */
   @Override
   public boolean checkContainsDateRange(LocalDate startDate, LocalDate endDate) {
     return getReader().checkContainsDateRange(startDate, endDate);
   }
 
+  /**
+   * Gets the most recent date of the stock.
+   * @return the most recent date of the stock
+   */
   @Override
   public LocalDate getMostRecentDate() {
     return getReader().getMostRecentDate();
   }
 
+  /**
+   * Gets the open price of the stock.
+   * @param date the date of the open price
+   * @return the open price of the stock
+   */
   @Override
   public double getOpenPrice(LocalDate date) {
     return getStockData(date, StockDataPoint.OPEN);
   }
 
+  /**
+   * Gets the close price of the stock.
+   * @param date the date of the close price
+   * @return the close price of the stock
+   */
   @Override
   public double getClosePrice(LocalDate date) {
     return getStockData(date, StockDataPoint.CLOSE);
   }
 
+  /**
+   * Gets the high price of the stock.
+   * @param date the date of the high price
+   * @return the high price of the stock
+   */
   @Override
   public double getHighPrice(LocalDate date) {
     return getStockData(date, StockDataPoint.HIGH);
   }
 
+  /**
+   * Gets the low price of the stock.
+   * @param date the date of the low price
+   * @return the low price of the stock
+   */
   @Override
   public double getLowPrice(LocalDate date) {
     return getStockData(date, StockDataPoint.LOW);
   }
 
+  /**
+   * Gets the volume of the stock.
+   * @param date the date of the volume
+   * @return the volume of the stock
+   */
   @Override
   public double getVolume(LocalDate date) {
     return getStockData(date, StockDataPoint.VOLUME);
   }
 
+  /**
+   * Gets the stock data for the given date and data point.
+   * @param date the date of the stock data
+   * @param dataPoint the data point to get
+   * @return the stock data for the given date and data point
+   */
   private double getStockData(LocalDate date, StockDataPoint dataPoint) {
     return getReader().getStockData(date, dataPoint);
   }
 
+  /**
+   * Gets the data across the given number of days.
+   * @param endDate the end date of the data
+   * @param days the number of days to get data across
+   * @param dataPoint the data point to get
+   * @return the data across the given number of days
+   */
   @Override
   public List<String> getDataAcrossDays(LocalDate endDate, int days,
                                         StockDataPoint dataPoint) {
     return getReader().getDataAcrossDays(endDate, days, dataPoint);
   }
 
+  /**
+   * Gets the data across the given number of days.
+   * @param startDate the start date of the data
+   * @param endDate the end date of the data
+   * @param dataPoint the data point to get
+   * @return the data across the given number of days
+   */
   @Override
   public List<String> getDataAcrossDays(LocalDate startDate, LocalDate endDate,
                                         StockDataPoint dataPoint) {
     return getReader().getDataAcrossDays(startDate, endDate, dataPoint);
   }
 
+  /**
+   * Gets the data across the given number of days.
+   * @param obj the object to compare
+   * @return true if the objects are equal, false otherwise
+   */
   @Override
   public boolean equals(Object obj) {
     if (this == obj) {
@@ -91,6 +173,10 @@ public class Stock implements IStock {
     return ticker.equals(stock.ticker);
   }
 
+  /**
+   * Gets the hash code of the stock.
+   * @return the hash code of the stock
+   */
   @Override
   public int hashCode() {
     return Objects.hash(ticker);
