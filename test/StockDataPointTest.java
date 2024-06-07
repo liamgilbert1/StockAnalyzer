632a4635a4229a02d944931ba36eca3d783de073
Joseph-Carlucci <148010329+Joseph-Carlucci@users.noreply.github.com>
2024-06-07 15:42:31 -0400

    Empty Test Files added

---
+++ test/StockDataPointTest.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StockDataPointTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void getIndex() {
+  }
+
+  @Test
+  public void values() {
+  }
+
+  @Test
+  public void valueOf() {
+  }
+}
