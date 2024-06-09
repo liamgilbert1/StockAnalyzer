3fc4e1edcaec2a3ca8e41532cffb1e3d2116c823
Joseph-Carlucci <148010329+Joseph-Carlucci@users.noreply.github.com>
2024-06-09 14:35:26 -0400

    commit

--- src/controller/commands/newCommands/PerformanceOverTimeCommand.java
+++ src/controller/commands/newCommands/PerformanceOverTimeCommand.java
@@ -1,7 +1,23 @@
 package controller.commands.newCommands;
 
+import java.util.Scanner;
+
 import controller.commands.AWriterCommand;
+import model.IModel;
 
 public class PerformanceOverTimeCommand extends AWriterCommand {
 
+  public PerformanceOverTimeCommand(Appendable out) {
+    super(out);
+  }
+
+  @Override
+  public void execute(IModel model, Scanner scanner) {
+
+  }
+
+  @Override
+  public String getInstructions() {
+    return null;
+  }
 }
