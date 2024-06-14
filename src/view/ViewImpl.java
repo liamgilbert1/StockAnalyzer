b7f7d1af40c3a4f01361edc6cdceeef67243d894
Joseph-Carlucci <148010329+Joseph-Carlucci@users.noreply.github.com>
2024-06-14 12:16:39 -0400

    view edit

--- src/view/ViewImpl.java
+++ src/view/ViewImpl.java
@@ -11,18 +11,18 @@ public class ViewImpl implements IView {
   @Override
   public Appendable append(CharSequence csq) throws IOException {
     output.append(csq);
-    return output;
+    return this;
   }
 
   @Override
   public Appendable append(CharSequence csq, int start, int end) throws IOException {
     output.append(csq.subSequence(start, end));
-    return output;
+    return this;
   }
 
   @Override
   public Appendable append(char c) throws IOException {
     output.append(c);
-    return output;
+    return this;
   }
 }
